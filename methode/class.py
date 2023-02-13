c2818617c25c4a87214cf17fba222eec76d1a779
naftali2055 <naftali2055@gmail.com>
2023-02-13 16:57:05 +0200

    add student gui and class studemt, study class

---
+++ methode/class.py
@@ -0,0 +1,27 @@
+class Class:
+    def __init__(self, class_id) -> None:
+        self.class_id = class_id
+
+    @property
+    def study_class(self) -> str:
+        pass
+
+    @study_class.setter
+    def study_class(self) -> None:
+        pass
+
+    @property
+    def add_class(self) -> str:
+        pass
+
+    @add_class.setter
+    def add_class(self, class_id) -> None:
+        pass
+
+    @property
+    def students(self) -> str:
+        pass
+
+    @students.setter
+    def students(self, studentID):
+        pass
